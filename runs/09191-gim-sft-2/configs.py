2e2f2b614d86957edf6ae5dbd1eb66a5eeac6efa
Shichao Song <60967965+Ki-Seki@users.noreply.github.com>
2025-09-19 14:34:19 +0800

    feat: add training and configuration scripts for new run (#13)
    * feat: add training and configuration scripts for new run
    
    * feat: update artifact directory structure and logging configuration

---
+++ runs/09191-gim-sft-2/configs.py
@@ -0,0 +1,26 @@
+import pathlib
+
+import torch
+
+
+EXP_NAME = pathlib.Path(__file__).resolve().parent.name
+
+ARTIFACTS_DIR = pathlib.Path(__file__).resolve().parent.parent.parent / "artifacts" / EXP_NAME
+FINAL_MODEL_DIR = ARTIFACTS_DIR / "sft-gim"
+ARTIFACTS_DIR.mkdir(parents=True, exist_ok=True)
+
+RANDOM_SEED = 0
+
+DATASET_LEN = 50000
+TRAIN_SPLIT = 0.98
+TRAIN_SIZE = int(DATASET_LEN * TRAIN_SPLIT)
+
+NUM_GPUS = torch.cuda.device_count()
+MICRO_BSZ = 8
+GRAD_ACCUM = 4
+GLOBAL_BSZ = MICRO_BSZ * GRAD_ACCUM * NUM_GPUS
+
+ESTIMATED_STEPS = (TRAIN_SIZE // GLOBAL_BSZ) * 1
+WARMUP_STEPS = max(1, ESTIMATED_STEPS // 20)
+SAVE_STEPS = max(1, ESTIMATED_STEPS // 10)
+EVAL_STEPS = SAVE_STEPS
