8822e63110ccbc32d001a4cb778cf576254fa6ad
Shichao Song <60967965+Ki-Seki@users.noreply.github.com>
2025-10-04 00:04:06 +0800

    refactor: rename process_raw_response to infill_responses and update related function calls

--- src/gimkit/models/utils.py
+++ src/gimkit/models/utils.py
@@ -27,15 +27,14 @@ def build_cfg(query: Query) -> CFG:
         if num_tags > 0
         else ""
     )
-    output_type = CFG(f"{grammar_first_line}\n{grammar_next_lines}")
-    return output_type
+    return CFG(f"{grammar_first_line}\n{grammar_next_lines}")
 
 
 def build_json_schema(query: Query) -> JsonSchema:  # pragma: no cover  # TODO
     raise NotImplementedError("JSON schema generation is not implemented yet.")
 
 
-def get_output_type(
+def get_outlines_output_type(
     output_type: Literal["cfg", "json"] | None, query: Query
 ) -> None | CFG | JsonSchema:
     if output_type is None:
@@ -53,12 +52,12 @@ def transform_to_outlines(
     output_type: Literal["cfg", "json"] | None,
 ):
     query_obj = Query(model_input) if not isinstance(model_input, Query) else model_input
-    outlines_output_type = get_output_type(output_type, query_obj)
     outlines_model_input = str(query_obj)
-    return outlines_output_type, outlines_model_input
+    outlines_output_type = get_outlines_output_type(output_type, query_obj)
+    return outlines_model_input, outlines_output_type
 
 
-def process_raw_response(
+def infill_responses(
     query: ContextInput | Query, responses: str | list[str] | Any
 ) -> Result | list[Result]:
     if isinstance(responses, str):
@@ -83,11 +82,11 @@ def _call(
     backend: str | None = None,
     **inference_kwargs: Any,
 ) -> Result | list[Result]:
-    outlines_output_type, outlines_model_input = transform_to_outlines(model_input, output_type)
+    outlines_model_input, outlines_output_type = transform_to_outlines(model_input, output_type)
     raw_response = Generator(self, outlines_output_type, backend)(
         outlines_model_input, **inference_kwargs
     )
-    return process_raw_response(model_input, raw_response)
+    return infill_responses(model_input, raw_response)
 
 
 async def _acall(
@@ -97,7 +96,7 @@ async def _acall(
     backend: str | None = None,
     **inference_kwargs: Any,
 ) -> Result | list[Result]:
-    outlines_output_type, outlines_model_input = transform_to_outlines(model_input, output_type)
+    outlines_model_input, outlines_output_type = transform_to_outlines(model_input, output_type)
     generator = Generator(self, outlines_output_type, backend)
-    raw_response = await generator(outlines_model_input, **inference_kwargs)
-    return process_raw_response(model_input, raw_response)
+    raw_responses = await generator(outlines_model_input, **inference_kwargs)
+    return infill_responses(model_input, raw_responses)
