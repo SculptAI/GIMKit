1d91adec135967d8a2cab38c34746a09db3fcb49
Shichao Song <60967965+Ki-Seki@users.noreply.github.com>
2025-10-03 18:04:09 +0800

    test: update vllm_offline tests to include result handling and mock client

--- tests/models/test_vllm_offline.py
+++ tests/models/test_vllm_offline.py
@@ -1,10 +1,12 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 from outlines.models.vllm_offline import VLLMOffline as OutlinesVLLMOffline
 from vllm import LLM
 
+from gimkit.contexts import Result
 from gimkit.models.vllm_offline import VLLMOffline as GIMVLLMOffline
 from gimkit.models.vllm_offline import from_vllm_offline
+from gimkit.schemas import MaskedTag
 
 
 def test_from_vllm_offline():
@@ -15,6 +17,10 @@ def test_from_vllm_offline():
 
 
 def test_vllm_offline_call():
-    mock_model = MagicMock(spec=GIMVLLMOffline)
-
-    mock_model("Hi")
+    mock_client = MagicMock(spec=LLM)
+    model = from_vllm_offline(mock_client)
+    result = Result(MaskedTag(id=0, content="hi"))
+    with patch("gimkit.models.vllm_offline._call", return_value=result) as mock_call:
+        result = model(MaskedTag())
+        assert result.tags[0].content == "hi"
+        mock_call.assert_called_once_with(model, MaskedTag(), "cfg", None)
