823819a0424dc681fb55e93f837268b68da9ec25
Shichao Song <60967965+Ki-Seki@users.noreply.github.com>
2025-10-07 14:36:50 +0800

    style: reformat

--- tests/test_gimkit.py
+++ tests/test_gimkit.py
@@ -9,4 +9,7 @@ def test_gimkit_version():
     if v != "unknown":
         # PEP 440 compliance check
         # Ref: https://peps.python.org/pep-0440/
-        assert re.match(r'^([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?$', v)
+        assert re.match(
+            r"^([1-9][0-9]*!)?(0|[1-9][0-9]*)(\.(0|[1-9][0-9]*))*((a|b|rc)(0|[1-9][0-9]*))?(\.post(0|[1-9][0-9]*))?(\.dev(0|[1-9][0-9]*))?$",
+            v,
+        )
